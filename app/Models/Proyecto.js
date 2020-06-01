f0c7fa14c26187409e8784423f683591a521b63a
Jorge Rodriguez <jorgeedison1@gmail.com>
2020-05-31 21:28:20 -0500

    Modelo Proyecto

--- app/Models/Proyecto.js
+++ app/Models/Proyecto.js
@@ -4,6 +4,14 @@
 const Model = use('Model')
 
 class Proyecto extends Model {
+  user () {
+    return this.belongsTo('App/Models/User')
+  }
+
+  tareas () {
+    return this.hasMany('App/Models/Tarea')
+  }
+
 }
 
 module.exports = Proyecto
