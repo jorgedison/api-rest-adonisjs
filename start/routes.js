9d3e501fab0d2dbf8f9dd9b0b317873dd3a6f213
Jorge Rodriguez <jorgeedison1@gmail.com>
2020-06-02 23:00:50 -0500

    Comentario rutas

--- start/routes.js
+++ start/routes.js
@@ -3,9 +3,11 @@
 const Route = use('Route')
 
 Route.group(() => {
+  //rutas usuarios
   Route.post('usuarios/', 'UserController.store');
   Route.post('usuarios/login', 'UserController.login');
   Route.get('usuarios/', 'UserController.index');
+  //rutas proyectos
   Route.get('proyectos', 'ProyectoController.index').middleware('auth');
   Route.post('proyectos', 'ProyectoController.create').middleware('auth');
   Route.patch('proyectos/:id', 'ProyectoController.update').middleware('auth');
